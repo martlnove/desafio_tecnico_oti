ddaf25acd743c097658737afccf920c30d93b850
Lucas Martins <lucasshc@gmail.com>
2021-09-04 02:21:24 -0300

    update bootstrap

--- desafio_oti/src/App.vue
+++ desafio_oti/src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="produto of produtos">
-        <img :src="produto.url" :alt="produto.titulo">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Desafio Técnico OTI</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Digite seu produto" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Buscar</button>
+           </form>
+         </div>
+      </div>
+    </nav>
+    <div class="alert alert-dark" role="alert">
+      <h1 class="alert-heading">{{titulo}}</h1>
+      <hr>
+      <p>{{mensagem}}</p>
+    </div>
+    <ul class="text-center lista">
+      <li class="rounded lista-produto" v-for="produto of produtos">
+        <div class="painel">
+          <h2 class="painel-titulo">{{produto.titulo}}</h2>
+          <img class="img-fluid size" :src="produto.url" :alt="produto.titulo">
+        </div>
       </li>
     </ul>
   </div>
@@ -13,14 +31,15 @@
 export default {
   data() {
     return{
-      titulo: 'Eletrodomesticos',
+      titulo: 'Nossos Produtos',
+      mensagem: 'Aqui você confere todos os nossos eletrodomésticos para sua residência',
       produtos: [
         {
-          url: 'https://brastemp.vtexassets.com/arquivos/ids/213732/Geladeira-BRE80AK-Frontal.jpg?v=637298140570900000',
-          titulo: 'Geladeira'
+          url: 'https://consul.vtexassets.com/arquivos/ids/218419-800-auto?width=800&height=auto&aspect=true',
+          titulo: 'Geladeira',
         },
         {
-          url: 'https://m.media-amazon.com/images/I/81m5hJSkwiL._AC_SY679_.jpg',
+          url: 'https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=149305041',
           titulo: 'Liquidificador'
         },
         {
@@ -28,12 +47,24 @@ export default {
           titulo: 'Batedeira'
         },
         {
-          url: 'https://http2.mlstatic.com/D_NQ_NP_813885-MLA31454776804_072019-O.jpg',
+          url: 'https://d296pbmv9m7g8v.cloudfront.net/Custom/Content/Products/99/39/993960_microondas-panasonic-style-nnst67hs-900w-32-litros-10003980_m53_637475373249592555',
           titulo: 'Microondas'
         },
         {
           url: 'https://novomundo.vteximg.com.br/arquivos/ids/1617816-500-500/fogao-5-bocas-de-piso-atlas-acendimento-automatico-tripla-chama-preto-tropical-glass-19-bivolt-58683-0.jpg?v=636994036049930000',
           titulo: 'Fogão'
+        },
+        {
+          url: 'https://images-americanas.b2w.io/produtos/01/00/img/134230/1/134230135_1GG.jpg',
+          titulo: 'Air Fryer'
+        },
+        {
+          url: 'http://philco.vteximg.com.br/arquivos/ids/175013/Torradeira-BTR01P_066201023--3-.jpg?v=637038018154730000',
+          titulo: 'Torradeira'
+        },
+        {
+          url: 'https://a-static.mlcdn.com.br/618x463/ventilador-arno-40cm-turbo-force-vf49-vf49/gazinshop/8931/097736b9cdf3f254a28e9ca800790ecd.jpg',
+          titulo: 'Ventilador'
         }
 
       ]
@@ -43,5 +74,34 @@ export default {
 </script>
 
 <style>
+.lista {
+    list-style: none;
+  }
+.painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 300px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
 
+.painel .painel-titulo {
+    color: #FFF;
+    text-align: center;
+    border: solid 2px;
+    background: #003c86;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+.size{
+  width:100%;
+}
+.lista .lista-produto {
+    display: inline-block;
+}
 </style>
