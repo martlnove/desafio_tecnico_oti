1aa336b9f64ce41894c171f5c2bb0f3e4556a34d
Lucas Martins <lucasshc@gmail.com>
2021-09-04 11:15:05 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,34 +39,42 @@ export default {
       mensagem: 'Aqui você confere todos os nossos eletrodomésticos para sua residência',
       produtos: [
         {
+          id: 'prodgel',
           url: 'https://consul.vtexassets.com/arquivos/ids/218419-800-auto?width=800&height=auto&aspect=true',
           titulo: 'Geladeira',
         },
         {
+          id: 'prodliqui',
           url: 'https://www.extra-imagens.com.br/Control/ArquivoExibir.aspx?IdArquivo=149305041',
           titulo: 'Liquidificador'
         },
         {
+          id: 'prodbat',
           url: 'https://www.casasbahia-imagens.com.br/html/conteudo-produto/73/50004211/imagens/batedeira_2_.png',
           titulo: 'Batedeira'
         },
         {
+          id: 'prodmicro',
           url: 'https://d296pbmv9m7g8v.cloudfront.net/Custom/Content/Products/99/39/993960_microondas-panasonic-style-nnst67hs-900w-32-litros-10003980_m53_637475373249592555',
           titulo: 'Microondas'
         },
         {
+          id: 'prodofog',
           url: 'https://novomundo.vteximg.com.br/arquivos/ids/1617816-500-500/fogao-5-bocas-de-piso-atlas-acendimento-automatico-tripla-chama-preto-tropical-glass-19-bivolt-58683-0.jpg?v=636994036049930000',
           titulo: 'Fogão'
         },
         {
+          id: 'prodair',
           url: 'https://images-americanas.b2w.io/produtos/01/00/img/134230/1/134230135_1GG.jpg',
           titulo: 'Air Fryer'
         },
         {
+          id: 'prodtorra',
           url: 'http://philco.vteximg.com.br/arquivos/ids/175013/Torradeira-BTR01P_066201023--3-.jpg?v=637038018154730000',
           titulo: 'Torradeira'
         },
         {
+          id: 'prodvent',
           url: 'https://a-static.mlcdn.com.br/618x463/ventilador-arno-40cm-turbo-force-vf49-vf49/gazinshop/8931/097736b9cdf3f254a28e9ca800790ecd.jpg',
           titulo: 'Ventilador'
         }
